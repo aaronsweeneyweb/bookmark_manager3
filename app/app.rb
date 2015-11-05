4d01fc51964b5849fa99cbe94d2c40a2d98cc3b4
Aaron Sweeney <aaron.sweeney@ucdconnect.ie>
2015-11-05 15:30:17 +0000

    Challenge 18: Adding multiple tags to links

--- app/app.rb
+++ app/app.rb
@@ -19,11 +19,13 @@ class BookmarkManager < Sinatra::Base
   end
 
   post '/links' do
-    link = Link.create(title: params[:title], url: params[:url])
-    tag = Tag.create(name: params[:tag])
-    link.tags << tag
-    link.save
-    redirect '/links'
+   link = Link.create(url: params[:url], title: params[:title])
+   params[:tags].split.each do |tag|
+     link.tags << Tag.create(name: tag)
+   end
+   link.save
+   redirect to('/links')
+
   end
 
   get '/tags/:name' do
--- spec/features/adding_tags_spec.rb
+++ spec/features/adding_tags_spec.rb
@@ -1,13 +1,24 @@
 feature 'Tags' do
-  scenario 'adding a single tag to link' do
+  scenario 'adding a single tag to a link' do
     visit('/links/new')
     fill_in 'title', with: 'Amazon'
     fill_in 'url', with: 'www.amazon.co.uk'
-    fill_in 'tag', with: '#books'
+    fill_in 'tags', with: '#books'
     click_button('Submit')
-    link=Link.first
+    link = Link.first
     expect(page.status_code).to eq 200
     expect(link.tags.map(&:name)).to include('#books') #|tag| tag.name
+  end
+
+  scenario 'adding multiple tags to a link' do
+    visit('/links/new')
+    fill_in 'title', with: 'Amazon'
+    fill_in 'url', with: 'www.amazon.co.uk'
+    fill_in 'tags', with: '#books #shopping'
+    click_button('Submit')
+    link = Link.first
+    expect(page.status_code).to eq 200
+    expect(link.tags.map(&:name)).to include('#books', '#shopping')
 
   end
 end
